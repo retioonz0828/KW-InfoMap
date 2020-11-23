773e21e7fc4af994265d7932b1f0f65e1179e920
김성준 <retioonz@gmail.com>
2020-11-24 01:18:12 +0900

    renew main

--- src/Sidebar.js
+++
@@ -1,9 +0,0 @@
-var sidewrap=document.createElement("div");
-sidewrap.className="sidebar-wrapper";
-document.body.appendChild(sidewrap);
-
-var placesList = document.createElement("ul");
-placesList.id = "placesList";
-sidewrap.appendChild(placesList);
-
-var ps = new kakao.maps.services.Places(map);
