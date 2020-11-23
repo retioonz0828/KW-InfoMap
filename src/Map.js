3dd172c3e4260c5e0fb9c8e48bd245eb7159ec34
taeyoung <banes159@naver.com>
2020-11-23 18:48:41 +0900

    Add info btn

--- src/Map.js
+++ src/Map.js
@@ -119,13 +119,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_18.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_18.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">대학본부, 건축공학과</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 404-21</div>' +
-                '                <div><a href="pages/화도관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17563675" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/화도관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17563675" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -141,13 +142,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_07.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_07.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 14px;">전자정보공과대학, 공학대학</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-1</div>' +
-                '                <div><a href="pages/비마관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17561911" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/비마관.html"  class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17561911" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -162,13 +164,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_07.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_07.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">소프트웨어 융합 대학</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-1</div>' +
-                '                <div><a href="pages/새빛관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/715015774" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/새빛관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/715015774" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -183,13 +186,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_05.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_05.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">강당</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 21</div>' +
                 '                <div class="jibun ellipsis">(우)01890 (지번) 월계동 466</div>' +
-                '                <div><a href="pages/동해문화예술관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17564275" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/동해문화예술관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17564275"class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -204,13 +208,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_14.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_14.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">자연과학대학</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로6길 31</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-5</div>' +
-                '                <div><a href="pages/옥의관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17567827" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/옥의관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17567827" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -225,13 +230,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_16.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_16.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">인문대학 </div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 27-38</div>' +
                 '                <div class="jibun ellipsis">(우) 01891 (지번) 월계동 465-41</div>' +
-                '                <div><a href="pages/한울관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17566581" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/한울관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17566581" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -246,13 +252,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_03.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_03.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">로봇학부, 경영학부</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로1길 60</div>' +
                 '                <div class="jibun ellipsis">(우) 01890 (지번) 월계동 505-2</div>' +
-                '                <div><a href="pages/누리관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/12721052" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/누리관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/12721052" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -267,13 +274,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_15.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_15.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 14px;">전자정보공과대학,공학대학</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-1</div>' +
-                '                <div><a href="pages/참빛관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/27202326" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/참빛관.html" class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/27202326"  class="kwbtn"target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -288,14 +296,15 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_24.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_24.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">도서관, 푸드코트</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-1</div>' +
-                '                <div><a href="pages/80주년기념관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="pages/중앙도서관.html" target="_blank">중앙 도서관 안내</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/823736351" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/80주년기념관.html" class="kwbtn2" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="pages/중앙도서관.html" class="kwbtn2" target="_blank">중앙 도서관 안내</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/823736351"class="kwbtn2" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -310,13 +319,14 @@ function kwplace(kwpositions) {
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_06.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_06.jpg"width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">동아리실, 함지마루(학식)</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 20</div>' +
                 '                <div class="jibun ellipsis">(우) 01897 (지번) 월계동 447-41</div>' +
-                '                <div><a href="pages/복지관.html" target="_blank">안내 페이지</a></div>' +
-                '                <div><a href="https://place.map.kakao.com/17565051" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="pages/복지관.html"  class="kwbtn" target="_blank">안내 페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/17565051" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
@@ -326,17 +336,19 @@ function kwplace(kwpositions) {
             content = '<div class="ovr">' +
                 '    <div class="info">' +
                 '        <div class="title">' +
-                '            빛솔재(기숙사)' +
+                '            빛솔재' +
                 '            <div class="close" onclick="removeOverlay()" title="닫기"></div>' +
                 '        </div>' +
                 '        <div class="body">' +
                 '            <div class="img">' +
-                '                <img src="https://www.kw.ac.kr/ko/img/tour01_26.jpg" width="110" height="90">' +
+                '                <img src="https://www.kw.ac.kr/ko/img/tour01_26.jpg" width="140" height="140">' +
                 '           </div>' +
                 '            <div class="desc">' +
+                '                <div class="pur" style="font-weight: 700; font-size: 15px;">기숙사</div>' +
                 '                <div class="ellipsis">서울 노원구 광운로 21</div>' +
                 '                <div class="jibun ellipsis">(우) 01890 (지번) 월계동 500-40</div>' +
-                '                <div><a href="https://place.map.kakao.com/614748387" target="_blank" class="link">오시는 길</a></div>' +
+                '                <div><a href="https://dorm.kw.ac.kr/" class="kwbtn" target="_blank" class="link">기숙사 홈페이지</a></div>' +
+                '                <div><a href="https://place.map.kakao.com/614748387" class="kwbtn" target="_blank" class="link">오시는 길</a></div>' +
                 '            </div>' +
                 '        </div>' +
                 '    </div>' +
